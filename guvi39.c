ebcb6c587340ab6b6d3a4f3eb448f1631a31d4e9
Ajithkumar <46858602+Ajithkumar004@users.noreply.github.com>
2019-01-27 16:12:13 +0530

    Update guvi39.c

--- guvi39.c
+++ guvi39.c
@@ -2,21 +2,21 @@
 #include<conio.h>
 void main()
 {
-int a[20],max,i,n;
+int a[20],maximum,i,n;
 clrscr();
 scanf("%d",&n);
 for(i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
 }
-max=0;
+maximum=0;
 for(i=0;i<n;i++)
 {
-if(a[i]>max)
+if(a[i]>maximum)
 {
-max=a[i];
+maximum=a[i];
 }
 }
-printf("%d",max);
+printf("%d",maximum);
 getch();
 }
